0214d76b2da956a70079570d4579a5f079931d47
Michael <mikailrmachado@hotmail.com>
2024-05-26 19:44:43 +0200

    Add WAlert

--- src/App.js
+++ src/App.js
@@ -4,38 +4,44 @@ import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
-import { InfoAlert, ErrorAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 
 const App = () => {
-  const [AllLocations, setAllLocations] = useState([]);
-  const [ChangeNumberOfEvents, setChangeNumberOfEvents] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [changeNumberOfEvents, setChangeNumberOfEvents] = useState(32);
   const [events, setEvents] = useState([]);
-  const [CurrentCity, setCurrentCity] = useState("See all cities");
+  const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
   const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
 
   useEffect(() => {
-    const fetchData = async () => {
-      const allEvents = await getEvents();
-      const filteredEvents =
-        CurrentCity === "See all cities"
-          ? allEvents
-          : allEvents.filter((event) => event.location === CurrentCity);
-      setEvents(filteredEvents.slice(0, ChangeNumberOfEvents));
-      setAllLocations(extractLocations(allEvents));
-    };
-
+    if (navigator.onLine) {
+      setWarningAlert("");
+    } else {
+      setWarningAlert("Offline App");
+    }
     fetchData();
-  }, [CurrentCity, ChangeNumberOfEvents]);
+  }, [currentCity, changeNumberOfEvents]);
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents =
+      currentCity === "See all cities"
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, changeNumberOfEvents));
+    setAllLocations(extractLocations(allEvents));
+  };
 
   return (
     <div className="App">
       <div className="alerts-container">
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch
-        allLocations={AllLocations}
+        allLocations={allLocations}
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert}
         setErrorAlert={setErrorAlert}
